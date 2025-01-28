e4d9c3a59d2f6de1a724ebe1d185426a2ae73cc9
abiduke <abiduke@umich.edu>
2025-01-28 17:21:25 -0500

    made gdb even better

--- gdbpractice.cpp
+++ gdbpractice.cpp
@@ -15,11 +15,11 @@ int findMin(int arr[], int size) {
 
 
 int main() {
-  int myArray1[4] = {9494, -5849, 76949, -3838};
+  int myArray1[4] = {9494, 5849, 76949, 3838};
   int myArray2[5] = {5, 4, 3, 2, 1};
   int myArray3[20] = {4, 7, 5, 8, 4, 3, 9, 6, 4, 3, 5, 6, 8, 7, 5};
 
-  cout << "Min element of first array is: " << findMin(myArray1, 5) << endl;
+  cout << "Min element of first array is: " << findMin(myArray1, 4) << endl;
   cout << "Min element of second array is: " << findMin(myArray2, 5) << endl;
   cout << "Min element of third array is: " << findMin(myArray3, 20) << endl;
 }
